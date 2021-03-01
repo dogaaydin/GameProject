3f632a7849706077edd9e40e980a53c9f85aede8
dogaaydin <dogaaydin_@hotmail.com>
2021-03-01 16:56:12 +0300

    Add project files.

---
+++ GameProject/IUserValidationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    //kullanıcı doğrulama servisi
+    interface IUserValidationService
+    {
+        bool Validate(Gamer gamer);
+
+    }
+}
